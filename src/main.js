281145950809890de1b9f194f929e7594dce87ac
João Fernando <jotaefegds@gmail.com>
2024-07-26 17:46:32 -0300

    feat: add web ui

---
+++ src/main.js
@@ -0,0 +1,10 @@
+"use strict"
+
+import "./styles/global.css";
+import "./styles/header.css";
+import "./styles/cardsContainer.css";
+import "./styles/cards/userProfileCard.css";
+import "./styles/cards/historyCard.css";
+import "./styles/cards/fidelityCard.css";
+import "./styles/cards/graphCard.css";
+import "./styles/components.css";
